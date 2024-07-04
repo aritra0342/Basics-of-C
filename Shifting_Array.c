3edc11951424bab79d330f113e4587077218c25c
aritra0342 <144580613+aritra0342@users.noreply.github.com>
2024-07-04 12:39:26 +0530

    Update Shifting_Array.c

--- Shifting_Array.c
+++ Shifting_Array.c
@@ -1,1 +1,48 @@
+//right shift
+#include <stdio.h>
 
+void shift_array_right(int arr[], int size) {
+    if (size <= 1) return;
+    int last = arr[size - 1];
+    for (int i = size - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = last;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    shift_array_right(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
+// Left shift
+
+#include <stdio.h>
+
+void shift_array_left(int arr[], int size) {
+    if (size <= 1) return;
+    int first = arr[0];
+    for (int i = 0; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    arr[size - 1] = first;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    shift_array_left(arr, size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
